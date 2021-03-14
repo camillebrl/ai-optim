10d352984a7fd08032dce110f8629ddee7addaa8
Victor NEPVEU <victor.nepveu@imt-atlantique.net>
2021-03-14 09:54:11 +0100

    Add utils file

---
+++ projet/utils.py
@@ -0,0 +1,12 @@
+def findnth_left(haystack, needle, n):
+    parts = haystack.split(needle, n + 1)
+    if len(parts) <= n + 1:
+        return -1
+    return len(haystack) + 1 - len(parts[-1]) - len(needle)
+
+
+def findnth_right(haystack, needle, n):
+    parts = haystack.split(needle, n + 1)
+    if len(parts) <= n + 1:
+        return -1
+    return len(haystack) - len(parts[-1]) - len(needle)
