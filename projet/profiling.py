1eb851df928f3c70b10e879ff579513c4aefee21
Victor NEPVEU <victor.nepveu@imt-atlantique.net>
2021-03-17 14:32:46 +0100

    Modify profiling

--- projet/profiling.py
+++ projet/profiling.py
@@ -1,7 +1,8 @@
 import os
 import torch
 import torch.nn as nn
-import resnet
+import logging
+from pipeline import load_model_and_hyperparameters
 
 def count_conv2d(m, x, y):
     x = x[0] # remove tuple
@@ -80,10 +81,10 @@ def profile(model, input_size, custom_ops = {}):
     def add_hooks(m):
         if len(list(m.children())) > 0: return
         m.register_buffer('total_ops', torch.zeros(1))
-        m.register_buffer('total_params', torch.zeros(1))
+        # m.register_buffer('total_params', torch.zeros(1))
 
-        for p in m.parameters():
-            m.total_params += torch.Tensor([p.numel()]) / 2 # Division Free quantification
+        # for p in m.parameters():
+        #     m.total_params += torch.Tensor([p.numel()]) / 2 # Division Free quantification
 
         if isinstance(m, nn.Conv2d):
             m.register_forward_hook(count_conv2d)
@@ -106,33 +107,73 @@ def profile(model, input_size, custom_ops = {}):
     model(x)
 
     total_ops = 0
-    total_params = 0
+    # total_params = 0
     for m in model.modules():
         if len(list(m.children())) > 0: continue
         total_ops += m.total_ops
-        total_params += m.total_params
+        # total_params += m.total_params
 
-    return total_ops, total_params
+    return total_ops
+    # return total_ops, total_params
 
-def main():
-    # Resnet18 - Reference for CIFAR 10
-    ref_params = 5586981
-    ref_flops  = 834362880
-    # WideResnet-28-10 - Reference for CIFAR 100
-    # ref_params = 36500000
-    # ref_flops  = 10490000000
 
-    model = resnet.ResNet18()
-    print(model)
-    flops, params = profile(model, (1,3,32,32))
-    flops, params = flops.item(), params.item()
+#################################################
+################# IMPORTANT #####################
+#################################################
+# Il se peut que ça ne foncitonne pas le if (isinstance(m,nn.qqch));
+# Effectivement, on a des bottelneck avec resnet!
+# for m in self.model.modules():
+#     if isinstance(m, Bottleneck):
+#         convs = []
+#         for m_2 in m.modules():
+#             if isinstance(m_2, nn.Conv2d):
+#                 convs.append(m_2)
+#                 # take all 2Dconv of each block except for the last one
+#         self.target_modules.extend(convs[:-1])
 
-    score_flops = flops / ref_flops
-    score_params = params / ref_params
-    score = score_flops + score_params
-    print("Flops: {}, Params: {}".format(flops,params))
-    print("Score flops: {} Score Params: {}".format(score_flops,score_params))
-    print("Final score: {}".format(score))
 
-if __name__ == "__main__":
-    main()
+def num_of_params(model):
+    nb_params=model.numel()
+    for param in model.parameters():
+        # count number of 0, sum the total and remove it
+        if param is not None :
+            nb_params-=torch.sum((param == 0).int()).data[0]
+
+
+def main(dataset,model_type):
+    """[summary]
+
+    Args:
+        dataset ([string]): "cifar10" ou "cifar100"
+        model_type ([string]): "models_distilled", "models_quantized", "models_pruned" ou "models_regularized"
+    """
+    listed_dir = f"./{dataset}/models/{model_type}"
+
+    if dataset == "cifar10":
+        ref_params = 5586981
+        ref_flops  = 834362880
+        for model in os.listdir(listed_dir):
+            logging.info(f"profiling model in {model}")
+            model, hparams = load_model_and_hyperparameters(model, listed_dir, int(dataset[dataset.find("1"):]))
+
+            flops = profile(model, (1,3,32,32))
+            flops = flops.item()
+
+            params=model.numel()
+            for param in model.parameters():
+                # compte le nombre de 0, somme le total de 0 et on l'enlève du nombre de paramètres total
+                if param is not None :
+                    params-=torch.sum((param == 0).int()).data[0]
+            
+            if model_type == "models_quantized" or model_type == "models_distilled":
+                type_params = type(params)
+                params = params / (int(type_params[type_params.find("t")+1:])/hparams.nb_bits) # "t" de "Float"
+
+            score_flops = flops / ref_flops
+            score_params = params / ref_params
+            score = score_flops + score_params
+
+            ######### Il faut qu'on trouve un moyen d'enregister ça quelque part proprement #########
+            print("Flops: {}, Params: {}".format(flops,params))
+            print("Score flops: {} Score Params: {}".format(score_flops,score_params))
+            print("Final score: {}".format(score))
